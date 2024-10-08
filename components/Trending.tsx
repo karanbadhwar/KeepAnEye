274cacd309e421c04303a460ed1b7392932cc26b
karanbadhwar <karanbadhwar07@gmail.com>
2024-10-07 23:51:42 -0400

    Initial Commit

---
+++ components/Trending.tsx
@@ -0,0 +1,11 @@
+const Trending = () => {
+  return (
+    <>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+      </section>
+    </>
+  );
+};
+
+export default Trending;
